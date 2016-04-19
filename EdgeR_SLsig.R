5fe2c2c537bf06e834d6efdc515182a43606d97e
saralinker <sara.linker@gmail.com>
2016-04-19 13:38:08 -0700

    automatic push: Tue Apr 19 13:38:01 PDT 2016

--- EdgeR_SLsig.R
+++ EdgeR_SLsig.R
@@ -4,62 +4,103 @@ library(edgeR)
 library(scatterplot3d)
 library(fdrtool)
 library(ggplot2)
+### other function
+GLM <- function(dat, variable1, variable2 = NULL, prefit=FALSE){
+  if(is.null(variable2)){
+    design <- model.matrix(~variable1)
+    Coef = 2
+  }else{
+    design <- model.matrix(~variable2*variable1)
+    Coef = 4
+  }
+  cds <- DGEList(dat)
+  if (prefit == FALSE){
+    cds <- calcNormFactors(cds)
+    cds <- estimateGLMCommonDisp( cds )
+    cds <- estimateGLMTrendedDisp(cds)
+    fit <- glmQLFit(y=cds,design)
+  }
+  fit <- glmQLFit(y=cds,design)
+  lrt <- glmLRT(fit, coef=Coef)#interaction term = 4
+  edg <- data.frame(lrt$table)
+  edg <- edg[order(edg$PValue),]
+  f <- p.adjust(edg$PValue)
+  edg$f <- f
+  return(edg)
+}
+exact <- function(dat, variable, Pair){
+  ########################
+  ### Pre-process Data
+  ########################
+  cds <- DGEList(dat,group=group)
+  # Filter out really lowly detected reads (*from EdgeR tutorial)
+  cds <- cds[rowSums(1e+06 * cds$counts/expandAsMatrix(cds$samples$lib.size, dim(cds)) > 1) >= 3, ]
+  cds <- calcNormFactors( cds )
+  ########################
+  ### Estimate parameters from data
+  ########################
+  cds <- estimateCommonDisp( cds )
+  cds <- estimateTagwiseDisp( cds )
+  cds <- estimateTrendedDisp(cds)
+  ## Run the test
+  de.cmn <- exactTest( cds , pair = Pair)
+  ########################
+  ### Format Results
+  ########################
+  res <- as.data.frame(de.cmn$table)
+  res <- res[order(res$PValue),]
+  #f <- fdrtool(x=res$PValue,statistic="pvalue",plot=FALSE)
+  res$f <- p.adjust(res$PValue, method = "fdr")
+  return(res)
+}
 ########################
 ###Load and Format Data
 ########################
 #save(list = c("activitygenes","celltypegenes","celltypegenes.hdg", "celltypegenes.dg", "celltypegenes.ca1", "celltypegenes.neg","celltypegenes.ca23","celltypegenes.in","activitygenes.ca1","activitygenes.dg","activitygenes.hdg","activitygenes.neg","RES"),file = "~/Documents/SalkProjects/ME/ShortLongSingature/SLSig_R/edgeR_slsig.rda",compress = TRUE)
 #load("~/Documents/SalkProjects/ME/ShortLongSingature/SLSig_R/edgeR_slsig.rda")
 ###
-samples <- metaProxC[metaProxC$subgroup == "CA2_3" & metaProxC$FOS != "L" & metaProxC$Mouse_condition == "HC" & metaProxC$alignable >  500000 & metaProxC$Smartseq2_RT_enzyme_used == "ProtoscriptII" |
-                       metaProxC$subgroup == "CA2_3" & metaProxC$FOS != "L"  & metaProxC$Mouse_condition == "HC" & metaProxC$alignable >  500000 & metaProxC$Smartseq2_RT_enzyme_used == "ProtoscriptII"  ,"Sample_ID"]
+samples <- metaProxC[metaProxC$Brain_Region == "CA1"  &  metaProxC$alignable >  500000 & metaProxC$Smartseq2_RT_enzyme_used == "ProtoscriptII" ,"Sample_ID"]#|
+                       #metaProxC$subgroup == "CA2_3" & metaProxC$FOS != "L"  & metaProxC$Mouse_condition == "HC" & metaProxC$alignable >  500000 & metaProxC$Smartseq2_RT_enzyme_used == "ProtoscriptII"  ,"Sample_ID"]
 dat <- na.exclude(countProxC[, samples])
 met <- metaProxC[match(samples,metaProxC$Sample_ID),]
-#Take a quick look through met to make sure everything is kosher
-met 
+table(data.frame(met$FOS, met$Mouse_condition))
+###################
 #Assign groups
+###################
+##### By exclusion 
 #outliers <- c("X151221_14" ,"X151221_15" ,"X151221_12", "X151221_10")
-group <- rep(TRUE,length(samples))
-group[match(outliers,samples)] <- FALSE
+#group <- rep(TRUE,length(samples))
+#group[match(outliers,samples)] <- FALSE
 #group[match(outliers,samples)] <- "a"
 #group[match(outliers2,samples)] <- "b"
 #remove <- which(group == "TRUE")
 #dat<- dat[,-c(remove)]
 #group <- as.factor(group[group != "TRUE"])
-#group <- met$FOS
+##### Or by meta 
+group <- as.factor(paste(met$FOS,met$Mouse_condition,sep = "."))
 Pair <- levels(as.factor(as.character(group)))
 
-########################
-### Pre-process Data
-########################
-cds <- DGEList(dat,group=group)
-# Filter out really lowly detected reads (*from EdgeR tutorial)
-cds <- cds[rowSums(1e+06 * cds$counts/expandAsMatrix(cds$samples$lib.size, dim(cds)) > 1) >= 3, ]
-cds <- calcNormFactors( cds )
-########################
-### Estimate parameters from data
-########################
-cds <- estimateCommonDisp( cds )
-cds <- estimateTagwiseDisp( cds )
-cds <- estimateTrendedDisp(cds)
-## Run the test
-de.cmn <- exactTest( cds , pair = Pair)
-########################
-### Format Results
-########################
-res <- as.data.frame(de.cmn$table)
-res <- res[order(res$PValue),]
-#f <- fdrtool(x=res$PValue,statistic="pvalue",plot=FALSE)
-res$f <- p.adjust(res$PValue, method = "fdr")
-#res.NE_NCA23_NNFvN <- res
+###################
+# Test genes
+###################
+#!!!Run exact test
+#res <- exact(dat, group, Pair)
+#!!!or glm
+res <- GLM(dat, group)
 
+res.HCNE_N_C_FN <- res
+
+############################
+### Keep track of gene lists 
+############################
 
 #edgenes <- c("Man1a","Bok","Pcp4","Bcl11b","Fgf2","Ntf3","Igfbp4","Trek2","Prox1","Egfr","Rbfox3")
 
 #Combine all results into a single list
 #RES <- list(res.HC_P_NvC_N,res.HC_PvsN_NvsC_N ,res.HC_PvsN_N_N ,res.HC_PvN_C_N ,res.HC_PvsN_CvsN_N ,res.HC_N_CvsN_N ,res.NE_N_N_FvN, res.NE_N_C_FvN, res.NE_P_C_FvN, res.NE_P_N_FvN)
 #names(RES) <- c("res.HC_P_NvC_N","res.HC_PvsN_NvsC_N" ,"res.HC_PvsN_N_N" ,"res.HC_PvN_C_N" ,"res.HC_PvsN_CvsN_N" ,"res.HC_N_CvsN_N" ,"res.NE_N_N_FvN", "res.NE_N_C_FvN", "res.NE_P_C_FvN", "res.NE_P_N_FvN")
-RES[[18]] <- res.NE_NCA23_NNFvN
-names(RES)[[18]] <- "res.NE_NCA23_NNFvN"
+RES[[(length(RES) + 1)]] <- res.HCNE_P_N_FN
+names(RES)[[(length(RES))]] <- "res.HCNE_P_N_FN"
 
 
 #### Find significant genes in each group
@@ -72,9 +113,9 @@ sum(RES[[i]]$f < 0.05 & RES[[i]]$logFC < 0)
 ## Get celltype-specific overlapping genes
 #################
 #pick groups with the pertinent comparisons
-group1 <- RES[[1]] 
-group2 <- RES[[2]]
-group3 <- RES[[3]]
+group1 <- RES[[2]] 
+group2 <- RES[[4]]
+group3 <- RES[[6]]
 
 nm <- table(c(rownames(group1),
               rownames(group2),rownames(group3)))
@@ -84,10 +125,14 @@ group2 <- group2[nm,]
 group3 <- group3[nm,]
 
 
-a <-rownames(group2[ group1$logFC < 0 & group1$f < 0.05 &
-                                     group2$logFC < 0 & group2$f < 0.05 &
-                                     group3$logFC < 0 & group3$f < 0.05  
+a <-rownames(group2[group1$logFC > 0 & group1$f < 0.05 &
+                                     group2$logFC > 0 & group2$f < 0.05 &
+                                     group3$logFC > 0 & group3$f < 0.05  
                                     ,])
+
+tmp <- group1[a,]
+tmp <- tmp[order(tmp$PValue),]
+
 
 celltypegenes.in <- a 
 celltypegenes.in <- c(a,celltypegenes.in)
