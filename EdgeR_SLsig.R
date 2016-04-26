3953270c9ac095262844f602c25d431763da6c1a
saralinker <sara.linker@gmail.com>
2016-04-26 13:38:10 -0700

    automatic push: Tue Apr 26 13:38:00 PDT 2016

--- EdgeR_SLsig.R
+++ EdgeR_SLsig.R
@@ -53,13 +53,14 @@ exact <- function(dat, variable, Pair){
   res$f <- p.adjust(res$PValue, method = "fdr")
   return(res)
 }
+
 ########################
 ###Load and Format Data
 ########################
 #save(list = c("activitygenes","celltypegenes","celltypegenes.hdg", "celltypegenes.dg", "celltypegenes.ca1", "celltypegenes.neg","celltypegenes.ca23","celltypegenes.in","activitygenes.ca1","activitygenes.dg","activitygenes.hdg","activitygenes.neg","RES"),file = "~/Documents/SalkProjects/ME/ShortLongSingature/SLSig_R/edgeR_slsig.rda",compress = TRUE)
 #load("~/Documents/SalkProjects/ME/ShortLongSingature/SLSig_R/edgeR_slsig.rda")
 ###
-samples <- metaProxC[metaProxC$Brain_Region == "DG" & metaProxC$alignable >  500000 & metaProxC$Smartseq2_RT_enzyme_used == "ProtoscriptII" ,"Sample_ID"]
+samples <- metaProxC[metaProxC$Brain_Region == "HDG" &  metaProxC$Mouse_condition == "HC" & metaProxC$FOS == "N" & metaProxC$alignable >  500000 & metaProxC$Smartseq2_RT_enzyme_used == "ProtoscriptII" ,"Sample_ID"]
 dat <- na.exclude(countProxC[, samples])
 met <- metaProxC[match(samples,metaProxC$Sample_ID),]
 table(data.frame(met$FOS, met$Mouse_condition))
@@ -67,8 +68,11 @@ table(data.frame(met$FOS, met$Mouse_condition))
 #Assign groups
 ###################
 ##### By exclusion 
-outliers <- met[c(1,7,6,14),"Sample_ID"]
-outliers2 <- met[c(4,11,3,2,5,9),"Sample_ID"]
+outliers <- c("X151221_03", "X151221_06","X151221_09","X151221_13",
+              "X151221_08","X151221_11")
+
+#outliers <- met[c(1,7,6,14),"Sample_ID"]
+#outliers2 <- met[c(4,11,3,2,5,9),"Sample_ID"]
 group <- rep(TRUE,length(samples))
 #group[match(outliers,samples)] <- FALSE
 group[match(outliers,samples)] <- "a"
@@ -79,6 +83,7 @@ group <- as.factor(group[group != "TRUE"])
 ##### Or by meta 
 group <- (paste(met$FOS,met$Mouse_condition,sep = "."))
 group[group != "N.EE"] <- FALSE
+group <- log(vip) < 6
 group <- as.factor(group)
 Pair <- levels(as.factor(as.character(group)))
 
@@ -87,7 +92,10 @@ Pair <- levels(as.factor(as.character(group)))
 ###################
 #!!!Run exact test
 res <- exact(dat, group, Pair)
+
+
 #!!!or glm
+
 #res <- GLM(dat, group)
 res.n.ee.left_vs_hc_N <- res.n.ee.right_vs_hc_N <- res
 
