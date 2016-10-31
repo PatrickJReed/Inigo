b0d30794cbd707f40c1ea84e66dbd6c6ab496efc
saralinker <sara.linker@gmail.com>
2016-10-31 13:38:11 -0700

    automatic push: Mon Oct 31 13:38:01 PDT 2016

--- SL_SCDE.R
+++ SL_SCDE.R
@@ -10,7 +10,9 @@ library(scde)
 ##############################################
 ## Step 1: Load in Count Table
 ##############################################
-samples <- rownames(metaProxC[metaProxC$Subgroup2 == "DG" & metaProxC$Mouse_condition == "EE" & metaProxC$FOS != "L"  & metaProxC$Arc_2.5 != "greater" & metaProxC$Context1 == "none" & metaProxC$Subgroup2!= "HDG" &  metaProxC$outliers == "in",])
+samples <- rownames(t[t$T1 < 0 & t$T2 > -10,])
+t.1 <- t[t$T1 < 0 & t$T2 > -10,]
+#samples <- rownames(metaProxC[metaProxC$FOS != "L"  & metaProxC$Arc_2.5 != "greater" & metaProxC$Context1 == "none" & metaProxC$cluster_outlier == "in" & metaProxC$outliers == "in",])
 dat <- na.exclude(countProxC[, samples])
 dat <- dat[rowSums(dat) > 0,]
 met <- metaProxC[samples,]
@@ -20,7 +22,7 @@ colnames(countTable2) <- colnames(dat)
 ###################
 #Assign groups
 ###################
-labels <- ifelse(met$FOS == "F" ,yes = "F",no = "N")
+labels <- ifelse(t[samples,"a"] == 2 ,yes = "F",no = "N")
 
 ##############################################
 ## Step 2: Fit error model
@@ -49,7 +51,6 @@ o.prior <- scde.expression.prior(models=o.ifm,
 ##############################################
 ## Step 3: Test Differential Expression
 ##############################################
-
 # define two groups of cells
 groups <- as.factor(labels)
 names(groups) <- row.names(o.ifm);
@@ -62,12 +63,11 @@ ediff <- scde.expression.difference(o.ifm,
                                     n.cores=n.cores,
                                     verbose=1)
 ediff2 <- ediff[order(ediff$Z,decreasing=T),]
-#ediff2$rank_high <- c(1:nrow(ediff2))
-#ediff2 <- ediff[order(ediff$Z,decreasing=F),]
-#ediff2$rank_low <- c(1:nrow(ediff2))
-ediff2$scde.f <- 2*pnorm(-abs(ediff2$cZ))
-tail(ediff2)
+ediff2$rank_high <- c(1:nrow(ediff2))
+ediff2 <- ediff[order(ediff$Z,decreasing=F),]
+ediff2$rank_low <- c(1:nrow(ediff2))
 
+tail(ediff2)
 # Plot posterior for a single gene
 
 gene <- "Arc"
